3663828a079aa313b7ed5d19e3d32d0c349d9461
Nick Palmer <ylnickpalmer@gmail.com>
2021-02-23 10:57:42 -0800

    added example.js to show call for Amazon API

--- example.js
+++ example.js
@@ -1,11 +1,11 @@
 //Example Call for Amazon API
 
-document.getElementById("exampleSearchBtn").addEventListener('click', event => {
+document.getElementById("searchBtn").addEventListener('click', event => {
   event.preventDefault()
   const options = {
     method: 'GET',
     url: 'https://amazon-price1.p.rapidapi.com/search',
-    params: { keywords: document.getElementById("search").value, marketplace: 'US' },
+    params: { keywords: document.getElementById("input").value, marketplace: 'US' },
     headers: {
       'x-rapidapi-key': '61a01e5c3amsh6da52702734f46ep192513jsncae7f7ec5553',
       'x-rapidapi-host': 'amazon-price1.p.rapidapi.com'
@@ -14,9 +14,8 @@ document.getElementById("exampleSearchBtn").addEventListener('click', event => {
 
   axios.request(options)
     .then(response => {
-      console.log(response.data);
+      console.log(response.data[0]);
       })
-    })
     .catch(error => {
       console.error(error);
     })
