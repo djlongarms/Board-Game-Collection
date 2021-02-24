4dfa7af3b2d99b3d084f17247255127176690261
Javier Ruiz <javierruizjr15@gmail.com>
2021-02-24 14:30:31 -0800

    java favs testing

--- app.js
+++ app.js
@@ -7,42 +7,88 @@ document.addEventListener('DOMContentLoaded', function () {
 
 // Script for favorites list++
 
-var boardTitles = []
-var nowTitle = "";
 
-// SavFav function created
-function SavFav() {
-  // savFav = () => {}     This is how u write the function with arrow
 
-  // store boardtitle under Test title
-  localStorage.setItem("Test title", boardTitles);
+let titleArray = [] || JSON.parse(localStorage.getItem("Game Title"))
 
-  // push nowTitle intow boardtitles array
-  boardTitles.push(nowTitle);
-
-  // convert to string
-  JSON.stringify(nowTitle);
-}
-
-// retreieve function created
-function retreiveFav() {
-
-  // retrieve
-  var savedFav = localStorage.getItem("Test title");
-
-  document.getElementById('favList').append(savedFav);
-}
-
-// create event upon click
 document.getElementById('addfavBtn').addEventListener('click', event => {
   event.preventDefault();
 
-  nowTitle = document.getElementById('wishlist').value
 
-  SavFav();
+  titleArray.forEach(element => {
 
-  retreiveFav();
+    let titleElem = document.createElement("p")
+    titleElem.textContent = element
+
+    document.getElementById('testTitle').append(titleElem)
+  });
+
+  // let favGamesArray = [] || JSON.parse(localStorage.getItem("Game Title"))
+
+  // axios.get('https://www.boardgameatlas.com/api/search?name=monopoly&limit=100&client_id=JLBr5npPhV')
+  //   .then(res => {
+  //     console.log(res.data.games[0].name)
+
+  //     // favGamesArray.push(res.data.games[0].name)
+  //     for (i = 0; i < res.data.games.length; i++) {
+  //       favGamesArray.push(res.data.games[i].name)
+
+  //     }
+
+  //     localStorage.setItem("Game Title", JSON.stringify(favGamesArray))
+
+
+  //     // localStorage.setItem("Game Title", res.data.games[0].name)
+
+
+  // })
 
 
 
+
+})
+
+// var boardTitles = []
+// var nowTitle = "";
+
+// SavFav function created
+// function SavFav() {
+// savFav = () => {}     This is how u write the function with arrow
+
+// push nowTitle intow boardtitles array
+// boardTitles.push(nowTitle);
+
+// convert to string, this needs to happen before local storage
+// JSON.stringify(nowTitle);
+
+// store boardtitle under Test title
+// localStorage.setItem("Test title", boardTitles);
+
+
+
+
+
+// }
+
+// retreieve function created
+// function retreiveFav() {
+
+// retrieve
+// var savedFav = localStorage.getItem("Test title");
+
+// document.getElementById('favList').append(savedFav);
+// }
+
+// create event upon click
+
+
+//   nowTitle = document.getElementById('wishlist').value
+
+
+
+
+
+  // SavFav();
+
+  // retreiveFav();
 
