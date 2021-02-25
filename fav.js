71b328401654336e46fcd590dbcbc7176a2bedea
Javier Ruiz <javierruizjr15@gmail.com>
2021-02-25 10:14:31 -0800

    name change

--- fav.js
+++ fav.js
@@ -1,9 +1,3 @@
-// script for nav bar icon++
-document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('.sidenav');
-  var instances = M.Sidenav.init(elems, {});
-});
-// end of script for nav bar--
 
 // Script for favorites list++
 
