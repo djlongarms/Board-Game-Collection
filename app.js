c74c8dc791605afb5307c4290e2b122b9dff7a82
Nick Palmer <ylnickpalmer@gmail.com>
2021-02-26 11:34:06 -0800

    reworked wishlist display, added display code

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ document.addEventListener('DOMContentLoaded', function () {
 // Gets wishlist array from localStorage
 let wishList = JSON.parse(localStorage.getItem('wishList')) || []
 
+// Listens for the click of an "Add to Wishlist" button
 document.addEventListener('click', event => {
   if (event.target.id === 'addWishBtn') {
     wishList.push(event.target.parentNode.childNodes[1].textContent)
@@ -15,10 +16,22 @@ document.addEventListener('click', event => {
   }
 })
 
+// Checks to see whether or not the wishList item is present on the page
 if(document.getElementById('wishList') !== null) {
+  // Cycles through the array of wishlist items
   wishList.forEach(element => {
-    let newWishItem = document.createElement("h5")
-    newWishItem.textContent = element
+    // Creates list item, gives it the proper class, then adds game name and a remove button
+    let newWishItem = document.createElement("li")
+    newWishItem.classList.add("collection-item")
+    newWishItem.innerHTML = `${element}<button id="removeWishBtn" type="submit"class="secondary-content">Remove</button>`
+
+    // Appends item to the wishlist on the page
     document.getElementById('wishList').append(newWishItem)
   })
 }
+
+document.addEventListener('click', event => {
+  if(event.target.id === "removeWishBtn") {
+    console.log("Working!")
+  }
+})
