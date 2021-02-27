46cb608b3e655e9eb657b78a47d905b4d1164f45
Nick Palmer <ylnickpalmer@gmail.com>
2021-02-27 14:31:22 -0800

    fixed links prevented from working by default

--- app.js
+++ app.js
@@ -8,14 +8,6 @@ document.addEventListener('DOMContentLoaded', function () {
 // Gets wishlist array from localStorage
 let wishList = JSON.parse(localStorage.getItem('wishList')) || []
 
-// Listens for the click of an "Add to Wishlist" button
-document.addEventListener('click', event => {
-  if (event.target.id === 'addWishBtn') {
-    wishList.push(event.target.parentNode.childNodes[1].textContent)
-    localStorage.setItem('wishList', JSON.stringify(wishList))
-  }
-})
-
 // Checks to see whether or not the wishList item is present on the page
 if(document.getElementById('wishList') !== null) {
   // Cycles through the array of wishlist items
@@ -23,7 +15,7 @@ if(document.getElementById('wishList') !== null) {
     // Creates list item, gives it the proper class, then adds game name and a remove button
     let newWishItem = document.createElement("li")
     newWishItem.classList.add("collection-item")
-    newWishItem.innerHTML = `${element}<button id="removeWishBtn" type="submit"class="secondary-content">Remove</button>`
+    newWishItem.innerHTML = `${element}<button name="removeWishBtn" type="submit"class="secondary-content">Remove</button>`
 
     // Appends item to the wishlist on the page
     document.getElementById('wishList').append(newWishItem)
@@ -31,20 +23,22 @@ if(document.getElementById('wishList') !== null) {
 }
 
 // Listens for click on the document of a "Remove" button in the wishlist
-document.addEventListener('click', event => {
+document.getElementsByName('removeWishBtn').forEach( element => {
+  element.addEventListener('click', event => {
   // Prevents default
-  event.preventDefault()
+    event.preventDefault()
 
-  // Checks to make sure "remove" button was clicked
-  if(event.target.id === "removeWishBtn") {
-    // Finds index of the item to be removed in the wishList array, then splices it out
-    let index = wishList.indexOf(event.target.parentNode.childNodes[0].data)
-    wishList.splice(index, 1)
-    
-    // Updates local storage to the new array
-    localStorage.setItem('wishList', JSON.stringify(wishList))
+    // Checks to make sure "remove" button was clicked
+    if(event.target.name === "removeWishBtn") {
+      // Finds index of the item to be removed in the wishList array, then splices it out
+      let index = wishList.indexOf(event.target.parentNode.childNodes[0].data)
+      wishList.splice(index, 1)
+      
+      // Updates local storage to the new array
+      localStorage.setItem('wishList', JSON.stringify(wishList))
 
-    // Removes element from the page
-    event.target.parentNode.remove(this)
-  }
+      // Removes element from the page
+      event.target.parentNode.remove(this)
+    }
+  })
 })
