b96d30171e3c13de1d834169550b43e0a5631e8c
Javier Ruiz <javierruizjr15@gmail.com>
2021-02-27 13:31:31 -0800

    favjava

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ document.addEventListener('click', event => {
 })
 
 // Checks to see whether or not the wishList item is present on the page
-if(document.getElementById('wishList') !== null) {
+if (document.getElementById('wishList') !== null) {
   // Cycles through the array of wishlist items
   wishList.forEach(element => {
     // Creates list item, gives it the proper class, then adds game name and a remove button
@@ -36,16 +36,63 @@ document.addEventListener('click', event => {
   event.preventDefault()
 
   // Checks to make sure "remove" button was clicked
-  if(event.target.id === "removeWishBtn") {
+  if (event.target.id === "removeWishBtn") {
     // Finds index of the item to be removed in the wishList array, then splices it out
     let index = wishList.indexOf(event.target.parentNode.childNodes[0].data)
     wishList.splice(index, 1)
     console
-    
+
     // Updates local storage to the new array
     localStorage.setItem('wishList', JSON.stringify(wishList))
 
     // Removes element from the page
     event.target.parentNode.remove(this)
   }
+})
+
+
+// FAVLIST
+// get favlist array
+let favlist = JSON.parse(localStorage.getItem('favlist')) || []
+
+// Listen for click of a add to favlist btn
+document.addEventListener('click', event => {
+  if (event.target.id === 'addfavBtn') {
+    favlist.push(event.target.parentNode.childNodes[1].tectContent)
+    localStorage.setItem('favList', JSON.stringify(favlist))
+  }
+})
+
+// check to see if favlist item is present on page
+if (document.getElementById('favlist') !== null) {
+  // cycle through fav list array
+  favlist.forEach(element => {
+    // create li, assigns class, add name, add remove btn
+    let newFavItem = document.createElement("li")
+    newFavItem.classList.add("collection-item")
+    newFavItem.innerHTML = `${element}<button id="removeFavBtn" type="submit" class="secondary-content">Remove</button>`
+
+    // appends item to favlist 
+    document.getElementById('favList').append(newFavItem)
+  })
+}
+
+// listen for click of a remove btn in the favlist
+document.addEventListener('click', event => {
+  // prevent default
+  event.preventDefault()
+
+  // checks to make sure remove btn was clicked
+  if (event.target.id === "removeFavBtn") {
+    // finds index of the item to be removed in the favlist array, then splices it out
+    let index = favlist.indexOf(event.target.parentNode.childNodes[0].data)
+    favlist.splice(index, 1)
+
+    // updates local storage tot he new array
+    localStorage.setItem('favlist', JSON.stringify(favlist))
+
+    // removes element from the page
+    event.target.parentNode.remove(this)
+  }
+
 })
--- fav.js
+++ fav.js
@@ -1,88 +1,100 @@
 
 // // Script for favorites list++
 
-// let titleArray = [] || JSON.parse(localStorage.getItem("Game Title"))
+// get favlist array
+let favlist = JSON.parse(localStorage.getItem('favlist')) || []
 
-let boardtitles = JSON.parse(localStorage.getItem("Game Title")) || [];
-
-console.log(boardtitles);
-let nowtitle = '';
-let displayli = '';
-
-
-
-// here is an example title pulled from atlas
-titledisplay = () => {
-  // access the atlas database
-  axios.get('https://www.boardgameatlas.com/api/search?name=scrabble&limit=100&client_id=JLBr5npPhV')
-
-    // display parameters I set
-    .then(res => {
-
-      // declare now title as title from API Database
-      let nowtitle = (res.data.games[0].name)
-      // console log test nowtitle
-      console.log(nowtitle)
-      // Display title from API data base on HTML displaytitle
-      document.getElementById('displaytitle').innerHTML = `<h4>${nowtitle}</h4>`;
-
-      // start the click event 
-      document.getElementById('addfavBtn').addEventListener('click', event => {
-
-        event.preventDefault();
-
-        // save game title from API to local storage
-        localStorage.setItem("Fav Game Title", nowtitle);
-        // stringify data
-        JSON.stringify(nowtitle);
-        // retrieve data from localsorage
-        let nowsavedtitle = localStorage.getItem("Game Title");
-        // console lof test nowsavedtitle
-        console.log(`this is it ${nowsavedtitle}`);
-
-        // push nowTitle intow boardtitles array
-        boardtitles.push(nowtitle).value
-
-        // save title array to localstorage
-        localStorage.setItem("Fav Game Title Array", boardtitles);
-
-        // strigify boardtitles
-        JSON.stringify(boardtitles);
-
-        // Retreve boardtitles and save retreved info to savedboardtitles
-        let savedboardtitles = localStorage.getItem("Fav Game Title Array");
-
-        console.log(`This is also it ${savedboardtitles}`)
-
-        displaytitleli = () => {
-
-          for (i = 0; i < boardtitles.length; i++)
-            displayli = boardtitles[i]
-        };
-
-        displaytitleli();
-
-        // declaring favElem to = an element <li>
-        let favElem = document.createElement('li')
-
-        // giving favElem created a class name of fav-list-item
-        favElem.className = 'fav-list-item'
-
-        // favElem will be the array savedboardtitles
-        favElem.textContent = displayli;
-
-        // add favElem to fav list id
-        document.getElementById('favList').append(favElem)
+// Listen for click of a add to wishlist btn
+document.addEventListener('click', event => {
+  if event.target.id === 'addfavBtn') {
+    favlist.push(event.target.parentNode.childNodes[1].tectContent)
+    localStorage.setItem('favList', JSON.stringify(favlist))
+  }
+})
 
 
-      })
 
-    })
+// another test...
+// let boardtitles = JSON.parse(localStorage.getItem("Game Title")) || [];
 
-  // for end of titledisplay function
-}
+// console.log(boardtitles);
+// let nowtitle = '';
+// let displayli = '';
 
-titledisplay();
+
+
+// // here is an example title pulled from atlas
+// titledisplay = () => {
+//   // access the atlas database
+//   axios.get('https://www.boardgameatlas.com/api/search?name=scrabble&limit=100&client_id=JLBr5npPhV')
+
+//     // display parameters I set
+//     .then(res => {
+
+//       // declare now title as title from API Database
+//       let nowtitle = (res.data.games[0].name)
+//       // console log test nowtitle
+//       console.log(nowtitle)
+//       // Display title from API data base on HTML displaytitle
+//       document.getElementById('displaytitle').innerHTML = `<h4>${nowtitle}</h4>`;
+
+//       // start the click event 
+//       document.getElementById('addfavBtn').addEventListener('click', event => {
+
+//         event.preventDefault();
+
+//         // save game title from API to local storage
+//         localStorage.setItem("Fav Game Title", nowtitle);
+//         // stringify data
+//         JSON.stringify(nowtitle);
+//         // retrieve data from localsorage
+//         let nowsavedtitle = localStorage.getItem("Game Title");
+//         // console lof test nowsavedtitle
+//         console.log(`this is it ${nowsavedtitle}`);
+
+//         // push nowTitle intow boardtitles array
+//         boardtitles.push(nowtitle).value
+
+//         // save title array to localstorage
+//         localStorage.setItem("Fav Game Title Array", boardtitles);
+
+//         // strigify boardtitles
+//         JSON.stringify(boardtitles);
+
+//         // Retreve boardtitles and save retreved info to savedboardtitles
+//         let savedboardtitles = localStorage.getItem("Fav Game Title Array");
+
+//         console.log(`This is also it ${savedboardtitles}`)
+
+//         displaytitleli = () => {
+
+//           for (i = 0; i < boardtitles.length; i++)
+//             displayli = boardtitles[i]
+//         };
+
+//         displaytitleli();
+
+//         // declaring favElem to = an element <li>
+//         let favElem = document.createElement('li')
+
+//         // giving favElem created a class name of fav-list-item
+//         favElem.className = 'fav-list-item'
+
+//         // favElem will be the array savedboardtitles
+//         favElem.textContent = displayli;
+
+//         // add favElem to fav list id
+//         document.getElementById('favList').append(favElem)
+
+
+//       })
+
+//     })
+
+//   // for end of titledisplay function
+// }
+
+// titledisplay();
 
 
 
